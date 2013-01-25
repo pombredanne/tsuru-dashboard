7b5ee9c4cd8d1a8df16183ff01309545965c61d1
Francisco Souza <f@souza.cc>
2013-01-25 17:15:31 -0200

    jobs: added milestones job
    This job fetches milestones information each ten minutes :-)

---
+++ jobs/milestones.rb
@@ -0,0 +1,30 @@
+# Copyright (c) 2013 tsuru authors
+# Use of this source code is governed by a BSD-style license that can be found
+# in the LICENSE file.
+
+require "json"
+require "net/http"
+
+milestones = [
+  {number: 13, id: "app-resilience"},
+  {number: 14, id: "tsuru-resilience"},
+]
+
+def fetch(milestones)
+  http = Net::HTTP.new "api.github.com", 443
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  milestones.each do |m|
+    response = http.get "/repos/globocom/tsuru/milestones/#{m[:number]}"
+    milestone = JSON.parse response.body
+    closed = milestone["closed_issues"]
+    total = closed + milestone["open_issues"]
+    percent = 100.0 * closed / total
+    send_event m[:id], { value: percent.ceil }
+  end
+end
+
+fetch milestones
+SCHEDULER.every "10m" do
+  fetch milestones
+end
