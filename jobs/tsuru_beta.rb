e4d210fb4c8afcc5a5affe47e7542ff5f0a2d593
Flavia Missi <flaviamissi@gmail.com>
2013-04-09 14:27:14 -0300

    jobs,dashboards: adding beta registered users count

---
+++ jobs/tsuru_beta.rb
@@ -0,0 +1,35 @@
+# Copyright (c) 2013 tsuru authors
+# Use of this source code is governed by a BSD-style license that can be found
+# in the LICENSE file.
+
+require "json"
+require "mongo"
+
+include Mongo
+
+def connect
+  if !ENV["MONGO_URI"] || !ENV["MONGO_DATABASE_NAME"]
+    throw "You must configure the mongodb variables in order to start the dashboard."
+  end
+  uri = ENV["MONGO_URI"].split(":")
+  user = ENV["MONGO_USER"]
+  password = ENV["MONGO_PASSWORD"]
+  database_name = ENV["MONGO_DATABASE_NAME"]
+  host, port = uri[0], uri[1].to_i
+  db = MongoClient.new(host, port).db(database_name)
+  if user && password
+    db.authenticate(user, password)
+  end
+  db
+end
+
+def fetch
+  widget_name = "tsuru-beta"
+  db = connect()
+  send_event widget_name, { current: db["users"].count }
+end
+
+fetch
+SCHEDULER.every '3m', :first_in => 0 do
+  fetch
+end
